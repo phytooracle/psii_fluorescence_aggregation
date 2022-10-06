ded2b9704d8adc4c9ac23528d1189e175f45c41b
Emmanuel Gonzalez <emmanuelgonzalez@email.arizona.edu>
2022-10-05 23:44:11 -0700

    update code

--- psii_fluorescence_aggregation.py
+++ psii_fluorescence_aggregation.py
@@ -10,6 +10,7 @@ Purpose: PS2 fluorescence aggregation
 """
 
 import argparse
+from operator import concat
 import os
 import sys
 import glob
@@ -60,74 +61,83 @@ def get_args():
 def generate_aggregate(csv):
     args = get_args()
 
-    try:
-        to_concat = []
+    to_concat = []
 
-        with open(args.multithresh) as f:
-            multithresh_list = json.loads(f.read())
+    with open(args.multithresh) as f:
+        multithresh_list = json.loads(f.read())
 
-        df = pd.read_csv(csv).drop('Unnamed: 0', axis=1)
-        sorted_df = df.sort_values(['Label', 'Max'])
+    df = pd.read_csv(csv).drop('Unnamed: 0', axis=1)
+
+    result_list = []
+
+
+    sorted_df = df.sort_values(['Label', 'Max'])
+    sorted_df['ID'] = sorted_df['Label'].str.split('_', expand=True)[0]
+    # print(len(sorted_df))
+
+    for image in sorted_df['ID'].unique():
+
+        temp_df = sorted_df[sorted_df['ID']==image]
 
         try:
             #sorted_df = df[['folder_name', 'Label', 'x', 'y', 'Area', 'Mean', 'Min', 'Max']]
-            sorted_df['MultiThr'] = multithresh_list
+            temp_df['MultiThr'] = multithresh_list
 
         except ValueError as e:
             # the length of multithresh list does not match the amound of images taken.
             # skip this
             print(csv, e)
             #continue
-        to_concat.append(sorted_df)
+        
+        to_concat.append(temp_df)
 
-        if to_concat:
-            concat_df = pd.concat(to_concat)
+    if to_concat:
+        concat_df = pd.concat(to_concat)
 
-        concat_df['AreaXMeanXMultiThr'] = concat_df['Area'] * concat_df['Mean'] * concat_df['MultiThr']
+    concat_df['AreaXMeanXMultiThr'] = concat_df['Area'] * concat_df['Mean'] * concat_df['MultiThr']
 
-        concat_df['AreaXMultiThr'] = concat_df['Area'] * concat_df['MultiThr']
+    concat_df['AreaXMultiThr'] = concat_df['Area'] * concat_df['MultiThr']
 
-        for label in set(concat_df['Label']):
-                #print(label, end='\r')
+    for label in set(concat_df['Label']):
+        #print(label, end='\r')
 
-                # creating a list of true/false to tell
-                # which records to select from concat_df
-                chosen_records = concat_df['Label'] == label
+        # creating a list of true/false to tell
+        # which records to select from concat_df
+        chosen_records = concat_df['Label'] == label
+        # print(chosen_records)
 
-                # Sum(Area x Mean x Multithresh)
-                concat_df.loc[chosen_records, 'Sum AreaXMeanXMultiThr'] = concat_df.loc[chosen_records, 'AreaXMeanXMultiThr'].sum()
+        # Sum(Area x Mean x Multithresh)
+        concat_df.loc[chosen_records, 'Sum AreaXMeanXMultiThr'] = concat_df.loc[chosen_records, 'AreaXMeanXMultiThr'].sum()
 
-                # Sum(Area x Multithresh)
-                concat_df.loc[chosen_records, 'Sum AreaXMultiThr'] = concat_df.loc[chosen_records, 'AreaXMultiThr'].sum()
+        # Sum(Area x Multithresh)
+        concat_df.loc[chosen_records, 'Sum AreaXMultiThr'] = concat_df.loc[chosen_records, 'AreaXMultiThr'].sum()
 
-        concat_df['Sum_AreaXMeanXMultiThr_over_Sum_AreaXMultiThr'] = concat_df['Sum AreaXMeanXMultiThr'] / concat_df['Sum AreaXMultiThr']
+    concat_df['Sum_AreaXMeanXMultiThr_over_Sum_AreaXMultiThr'] = concat_df['Sum AreaXMeanXMultiThr'] / concat_df['Sum AreaXMultiThr']
 
-        for label in set(concat_df['Label']):
+    for label in set(concat_df['Label']):
 
-            # the first value from the second picture
-            try:
-                F0 = list(concat_df.loc[concat_df['Label'] == label, 'Sum_AreaXMeanXMultiThr_over_Sum_AreaXMultiThr'])
-                #print(F0)
-            except IndexError as e:
-                # an IndexError will be thrown if no records were found for a given plot.
-                # this could happen if the plot was not listed in the Plot boundaries.xlsx
-                # and the previous step has been run
-                print(f'No data found in plot {label}.', e)
-                continue
+        # the first value from the second picture
+        try:
+            F0 = list(concat_df.loc[concat_df['Label'] == label, 'Sum_AreaXMeanXMultiThr_over_Sum_AreaXMultiThr'])
+            #print(F0)
+        except IndexError as e:
+            # an IndexError will be thrown if no records were found for a given plot.
+            # this could happen if the plot was not listed in the Plot boundaries.xlsx
+            # and the previous step has been run
+            print(f'No data found in plot {label}.', e)
+            continue
 
-            # extracting image number from the label string and converting it to an int for filtering
-            #concat_df['img_num'] = int(str(concat_df['Label']).split('_')[-2][-2:])
-            #print(concat_df['img_num'])
-            return concat_df
-    
-    except:
-        concat_df = pd.DataFrame()
-        return concat_df
+        # extracting image number from the label string and converting it to an int for filtering
+        #concat_df['img_num'] = int(str(concat_df['Label']).split('_')[-2][-2:])
+        #print(concat_df['img_num'])
+    return concat_df
 
 
 # --------------------------------------------------
 def generate_flurorescence(df):
     df_data = []
+
+    # print(df)
     for plot in set(df['Plot']):
 
         # the first value from the second picture
@@ -179,14 +189,12 @@ def main():
 
     if not file_list:
         file_list = [args.csv]
-
-    #print(args.csv)
-    #csv_list = glob.glob(args.csv)
+        
     concat_df = pd.DataFrame()
 
     with multiprocessing.Pool(multiprocessing.cpu_count()) as p:
         df = p.map(generate_aggregate, file_list)
-        #df = generate_aggregate(file_list, args.multithresh)
+        # df = generate_aggregate(file_list)#, args.multithresh)
         concat_df = concat_df.append(df)
 
     fluor_df = generate_flurorescence(concat_df)
